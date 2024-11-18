9af1e525dce5669f35ad0bcfc8fabfabf0e12183
Luominai <108951684+Luominai@users.noreply.github.com>
2024-11-17 19:20:26 -0500

    start with boardgame.io tutorial

---
+++ client/src/WhiteElephant.js
@@ -0,0 +1,18 @@
+export const TicTacToe = {
+    setup: () => ({ 
+        cells: Array(9).fill(null) 
+    }),
+
+    turn: {
+        minMoves: 1,
+        maxMoves: 1,
+    },
+
+    moves: {
+        
+    },
+
+    endIf: ({ G, ctx }) => {
+        
+    },
+};
