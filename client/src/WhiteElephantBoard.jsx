31912798b45ddf93b359e4fedebab6e6ef3f5d8f
Luominai <108951684+Luominai@users.noreply.github.com>
2024-11-19 14:39:28 -0500

    rows of 3 instead of 2. making space for the hand

--- client/src/WhiteElephantBoard.jsx
+++ client/src/WhiteElephantBoard.jsx
@@ -35,28 +35,46 @@ export function WhiteElephantBoard({ ctx, G, moves }) {
             </div>
             
             <div style={{
-                // height: "100vh",
-                width: "250px",
                 display: "flex",
-                alignItems: "start",
-                overflow:"scroll"
+                flexDirection: "column",
+                height: "100%"
             }}>
+
+                <div style={{
+                    height: "60vh",
+                    maxWidth: "380px",
+                    border: "1px solid lightgray",
+                    display: "flex",
+                    alignItems: "start",
+                    overflow:"scroll"
+                }}>
+                    <div style={{
+                        display: "flex",
+                        flexWrap: "wrap",
+                        gap: "5px",
+                        alignItems: "center",
+                        justifyContent: "center",
+                    }}>
+                        {G.gifts.map((gift, index) => {
+                            return (
+                                <div style={{cursor: "pointer"}} onClick={() => {
+                                    moves.stealGift(index)
+                                }}>
+                                    {Gift({gift: gift})}
+                                </div>
+                            )
+                        })}
+                    </div>
+                </div>
+
                 <div style={{
                     display: "flex",
-                    flexWrap: "wrap",
-                    gap: "5px",
-                    alignItems: "center",
-                    justifyContent: "center",
+                    width: "100%",
+                    backgroundColor: "lightblue",
+                    marginTop: "5%", marginBottom: "5%",
+                    flexGrow: 1
                 }}>
-                    {G.gifts.map((gift, index) => {
-                        return (
-                            <div style={{cursor: "pointer"}} onClick={() => {
-                                moves.stealGift(index)
-                            }}>
-                                {Gift({gift: gift})}
-                            </div>
-                        )
-                    })}
+                    hello
                 </div>
             </div>
         </div>
