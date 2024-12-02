36a1bbf42850d588143f679c93b6387807e34096
Luominai <108951684+Luominai@users.noreply.github.com>
2024-12-01 20:45:44 -0500

    trying out react-draggable

--- client/src/Card.jsx
+++ client/src/Card.jsx
@@ -1,36 +1,38 @@
-import { useState } from "react"
+import Draggable from "react-draggable"
+import { useRef } from "react"
 
 export default function Card({card}) {
-    const [style, setStyle] = useState({})
-    const [storedPos, setStoredPos] = useState({})
-    const [offset, setOffset] = useState({})
-    const [dragging, setDragging] = useState(false)
+    const threshold = useRef(null)
 
     return (
-        <div
-        onMouseEnter={() => {
-            setStyle({top: "-30px"})
+        <Draggable
+        position={{
+            x: 0, 
+            y: 0
         }}
-        onMouseLeave={() => {
-            setStyle({})
-            setDragging(false)
-        }}
-        onMouseDown={(e) => {
-            setStyle({position: "absolute", top: -30, left: 0})
-            setDragging(true)
-        }}
-        onMouseMove={(e) => {
-            if (dragging) {
-                console.log(e.movementX, e.movementY)
-                setStyle({...style, top: style.top += e.movementY, left: style.left += e.movementX})
+        onStop={(e, data) => {
+            // if the card has moved far enough to be in frame, use it
+            if (-1 * data.y >= threshold.current) {
+                console.log("card activate")
             }
         }}
+        onMouseDown={(e) => {
+            // get the position of mouse relative to the card
+            const rect = e.target.getBoundingClientRect();
+            const y = e.clientY - rect.top;
+            // do some math to figure out how much the card needs to move for the cursor to be in frame
+            // by default, the card is 10px away from the frame
+            threshold.current = 10 + y
+        }}
         >   
-            <div style={{
+            <div 
+            style={{
                 height: "150px",
                 width: "100px",
                 position: "relative",
-            }}>
+                draggable: true
+            }}
+            >
                 <div style={{
                     height: "150px",
                     width: "100px",
@@ -42,13 +44,11 @@ export default function Card({card}) {
                     transition: "top .2s",
                     top: "10px",
                     cursor: "pointer",
-                    ...style,
-                    ...offset
                 }}
                 >
                     {card.name}
                 </div>
             </div>
-        </div>
+        </Draggable>
     )
 }
--- client/src/WhiteElephantBoard.jsx
+++ client/src/WhiteElephantBoard.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import Gift from './Gift';
-import Card from './Card';
+import TurnOrder from './TurnOrder';
+import GiftPile from './GiftPile';
+import Hand from './Hand';
 
 export function WhiteElephantBoard({ ctx, G, moves, playerID}) {
     console.log(G.players, playerID)
@@ -12,86 +13,15 @@ export function WhiteElephantBoard({ ctx, G, moves, playerID}) {
             height: "98vh",
             marginTop: "2vh",
         }}>
-            <div style={{
-                // height: "100vh",
-                width: "20px",
-                textAlign: "start",
-                marginRight: "10px"
-            }}>
-                {ctx.playOrder.map((val, index) => {
-                    if (index === ctx.playOrderPos) {
-                        return (
-                            <div style={{color: "green", fontWeight: "bold"}}>
-                                {">" + val}
-                            </div>
-                        )
-                    }
-                    else {
-                        return (
-                            <div>
-                                {val}
-                            </div>
-                        )
-                    }
-                })}
-            </div>
+            <TurnOrder ctx={ctx}/>
             
             <div style={{
                 display: "flex",
                 flexDirection: "column",
                 height: "100%",
             }}>
-
-                <div style={{
-                    height: "80vh",
-                    width: "380px",
-                    border: "1px solid lightgray",
-                    display: "flex",
-                    alignItems: "start",
-                    justifyContent: "center",
-                    overflow:"scroll"
-                }}>
-                    <div style={{
-                        display: "flex",
-                        flexWrap: "wrap",
-                        gap: "5px",
-                        alignItems: "center",
-                        justifyContent: "center",
-                    }}>
-                        {G.gifts.map((gift, index) => {
-                            return (
-                                <div style={{cursor: "pointer"}} onClick={() => {
-                                    moves.stealGift(index)
-                                }}>
-                                    <Gift gift={gift}/>
-                                </div>
-                            )
-                        })}
-                    </div>
-                </div>
-
-                <div style={{
-                    display: "flex",
-                    width: "100%",
-                    // backgroundColor: "lightblue",
-                    marginBottom: "5%",
-                    flexGrow: 1,
-                    overflowY: "visible"
-                }}>
-                    <div style={{
-                        display: "grid",
-                        gridTemplateColumns: "repeat(auto-fit,minmax(10px,max-content))",
-                        width: "50%"
-                    }}>
-                        {G.players[playerID]?.map((card) => {
-                            return (
-                                <>
-                                    <Card card={card}/>
-                                </>
-                            )
-                        })}
-                    </div>
-                </div>
+                <GiftPile G={G} moves={moves}/>
+                <Hand G={G} playerID={playerID} moves={moves}/>
             </div>
         </div>
         </>
